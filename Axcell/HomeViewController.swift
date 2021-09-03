4f3e44180e1e5ce6fe89772c9848fe14e257fa27
Qasym Dar <qasymdar@gmail.com>
2021-09-03 18:33:53 -0500

    Finish HomeViewController, create new VCs for each sport, create segue to navigate to new Tennis VC

---
+++ Axcell/HomeViewController.swift
@@ -0,0 +1,44 @@
+//
+//  HomeViewController.swift
+//  Axcell
+//
+//  Created by Qasym Dar on 9/2/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    @IBAction func didTapSoccerBall(_ sender: Any) {
+    }
+    @IBAction func didTapFootball(_ sender: Any) {
+        
+        // segue to Football VC
+    }
+    
+    
+    @IBAction func didTapTennisBall(_ sender: Any) {
+        
+        // segue to Tennis VC
+        
+        let tennisVC = storyboard?.instantiateViewController(identifier: "TennisSetupVC")
+        
+        navigationController?.pushViewController(tennisVC!, animated: true)
+        
+        // present(tennisVC!, animated: true, completion: nil)
+        
+    }
+    
+    
+    
+}
+
