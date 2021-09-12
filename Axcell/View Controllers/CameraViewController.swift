427b10c3472f5d3ed288960e4bea323a606465a8
Qasym Dar <qasymdar@gmail.com>
2021-09-11 22:27:24 -0500

    Added live camera feature, instructions label, and Next button to CameraViewController

--- Axcell/View Controllers/CameraViewController.swift
+++ Axcell/View Controllers/CameraViewController.swift
@@ -6,20 +6,79 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
     
     var videoKey: String!
+    
+    var captureSession: AVCaptureSession!
+    var stillImageOutput: AVCapturePhotoOutput!
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
 
+
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var cameraView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .medium
         
+        guard let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+            else {
+                print("Unable to access back camera!")
+                return
+        }
+
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera)
+            //Step 9
+            
+            stillImageOutput = AVCapturePhotoOutput()
+            
+
+            if captureSession.canAddInput(input) && captureSession.canAddOutput(stillImageOutput) {
+                captureSession.addInput(input)
+                captureSession.addOutput(stillImageOutput)
+                setupLivePreview()
+            }
+        }
+        catch let error  {
+            print("Error Unable to initialize back camera:  \(error.localizedDescription)")
+        }
+
+        
+
     }
     
+    func setupLivePreview() {
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        videoPreviewLayer.videoGravity = .resizeAspect
+        videoPreviewLayer.connection?.videoOrientation = .portrait
+        cameraView.layer.addSublayer(videoPreviewLayer)
+        
+        //Step12
+        DispatchQueue.global(qos: .userInitiated).async { //[weak self] in
+            self.captureSession.startRunning()
+            //Step 13
+            DispatchQueue.main.async {
+                self.videoPreviewLayer.frame = self.cameraView.bounds
+            }
+        }
 
+    }
+
+    
+    @IBAction func didTapNext(_ sender: Any) {
+        captionLabel.text = "Recording started!!"
+    }
+    
     /*
     // MARK: - Navigation
 
